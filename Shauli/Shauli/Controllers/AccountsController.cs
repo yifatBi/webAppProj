07255486b0666acbf8aec227af7f30ae60c92ca8
Ido Cohen <305229742@stud.colman.ac.il>
2017-06-25 15:42:50 +0300

    Account model. with login/register controller and account manager controller. only admin can see manager link.

---
+++ Shauli/Shauli/Controllers/AccountsController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Shauli.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Shauli.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private AccountsDBContext db = new AccountsDBContext();
+
+        // GET: Accounts
+        public ActionResult Index()
+        {
+            return View(db.Accounts.ToList());
+        }
+
+        // GET: Accounts/Details/5
+        public ActionResult Details(int? Id)
+        {
+            if (Id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(Id);
+            if (account == null || !Id.ToString().Equals(Session["UserID"].ToString()))//Prevent url get details of another user.
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // GET: Accounts/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Accounts/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "Id,Usr,Password,ConfirmPassword,Email,Address,City,Country")] Account account)
+        {
+            try
+            {//check if user already exist...
+                var acc = db.Accounts.Single(u => u.Usr == account.Usr);
+                if (acc.Usr != null)
+                {
+                    ModelState.AddModelError("", "User already exist.");
+                    return View();
+                }
+
+            }
+            catch (InvalidOperationException e)
+            {//if acc is null do next..
+                if (ModelState.IsValid)
+                {
+                    db.Accounts.Add(account);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(account);
+        }
+
+        //login page.
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Account user)
+        {
+            try
+            {
+                var usr = db.Accounts.Single(u => u.Usr == user.Usr && u.Password == user.Password);
+                if (usr != null)
+                {
+                    Session["UserID"] = usr.Id.ToString();
+                    Session["Username"] = usr.Usr.ToString();
+                    Session["Admin"] = usr.IsAdmin.ToString();
+                    return RedirectToAction("index", "PostsToShow");
+                }
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError("", "Wrong username or password.");
+            }
+
+
+
+            return View();
+        }
+
+        public ActionResult LoggedIn()
+        {
+            if (Session["UserId"] != null)
+                return View();
+            else
+                return RedirectToAction("Login");
+        }
+
+        // GET: Accounts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Accounts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Usr,Password,ConfirmPassword,Email,Address,City,Country")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(account).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(account);
+        }
+
+        // GET: Accounts/Delete/5
+        public ActionResult Delete(int? Id)
+        {
+            if (Id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(Id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int Id)
+        {
+            Account account = db.Accounts.Find(Id);
+            db.Accounts.Remove(account);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Shauli/Shauli/Models/Account.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Shauli.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        public string Usr { get; set; }
+
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Street Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
+
+        public Boolean IsAdmin { get; set; }
+
+    }
+
+    public class AccountsDBContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
