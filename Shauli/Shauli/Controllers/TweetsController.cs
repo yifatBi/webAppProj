a4e288c961af734d1d37dd4be99f50cef8a4ca37
Ido Cohen <305229742@stud.colman.ac.il>
2017-06-25 23:01:02 +0300

    Tweeter working. only admins can tweet.

---
+++ Shauli/Shauli/Controllers/TweetsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Shauli.Models;
+
+namespace Shauli.Controllers
+{
+    public class TweetsController : Controller
+    {
+        private AccountsDBContext db = new AccountsDBContext();
+
+        public ActionResult Tweet()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Tweet(Tweets tweet)
+        {
+            if (Session["Admin"] != null && Session["Admin"].ToString().Equals("True") //Check if admin.
+                && tweet!=null&&tweet.TweetContent!=null)//check if tweet content is valid.
+            {
+                Tweetinvi.Auth.SetUserCredentials("OYTu2REUYtl1OvGlJuSD4Bkqv",
+                    "3z3Jwu7Wu1EBWRf6zrCRh4Hiv0qDQX9Y2fwkzNDnfN92xTzAfh",
+                    "879004861668478977-molXXNoVBwZOwgsLji5YXRRdGjsEXDg",
+                    "vifndBdgd216bqy7Gaj4KjrYWOiiQJU5v2cqeCbAB1lIl");
+                Tweetinvi.Tweet.PublishTweet(tweet.TweetContent);
+            }
+            return RedirectToAction("index", "PostsToShow");
+        }
+    }
+}
---
+++ Shauli/Shauli/Models/Tweets.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Shauli.Models
+{
+    public class Tweets
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public String TweetContent { get; set; }
+    }
+
+
+    public class TweetsDbContext : DbContext
+    {
+        public DbSet<Shauli.Models.Tweets> Tweets { get; set; }
+    }
+}
